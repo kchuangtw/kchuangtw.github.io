e9fbbbed1c3582d0cf1ea740c5dc6b226963372c
kchuangtw <69734403+kchuangtw@users.noreply.github.com>
2020-08-15 20:59:57 -0400

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,22 +1,21 @@
-/* Hide or display  */
 function formHandler() {
-  document.getElementById('radio').addEventListener('click', function(e) {
+  document.getElementById('radio').addEventListener('click', function (any) {
     try {
-      let element = document.getElementById(e.target.id);
-      /* Choose 'Order Problem' vs others*/
+      let element = document.getElementById(any.target.id);
       if (element.id === 'radio-3' && element.checked === true) {
         document.getElementById('order-num').required = true;
         document.getElementById('order-num').hidden = false;
         document.getElementById('order-num-label').hidden = false;
-      } else if (element.id === 'radio-1' || element.id === 'radio-2') {
+      }
+      else if (element.id === 'radio-1' || element.id === 'radio-2') {
         document.getElementById('order-num').required = false;
         document.getElementById('order-num').hidden = true;
         document.getElementById('order-num-label').hidden = true;
       }
-    } catch (e) {
+    }
+    catch (any) {
       return;
     }
   });
 }
-
 window.onload = formHandler;
